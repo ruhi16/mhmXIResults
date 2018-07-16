ad3e9b1610ee87c7411106a7bf6630ad83537d82
ruhi16 <hndas2016@gmail.com>
2018-07-17 00:49:15 +0530

    fc

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'ase64:ngv+Egti00PlJV3Qz01+0JFFfiI/u4OLLoScghUeQ48='),
 
     'cipher' => 'AES-256-CBC',
 
